36a28bec59d519cd1a767da0eb1b1946206392c8
jenn2512 <159705069+jenn2512@users.noreply.github.com>
2024-02-11 07:06:42 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const yesbtn = document.querySelector('#yesbtn');
+
+yesbtn.addEventListener('click', function () {
+    alert('Mmmm ya sabía que ibas a decir que si jiji uwu')
+});
+
+const nobtn = document.querySelector('#nobtn');
+
+nobtn.addEventListener('mouseover', function () {
+    const randomX = parseInt (Math.random()*100);
+    const randomY = parseInt (Math.random()*100);
+    nobtn.style.setProperty('top',randomY+'%');
+    nobtn.style.setProperty('left',randomX+'%');
+    nobtn.style.setProperty('transform', `translate(-${randomX}%,-${randomY}%)`);
+
+})
